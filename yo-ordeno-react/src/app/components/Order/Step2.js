2acbe23d115cbfb48e3910badb7fb11aef81d605
lockeas16 <lockeas16@gmail.com>
2019-06-27 20:49:40 -0500

    added filter component to customer view

--- yo-ordeno-react/src/app/components/Order/Step2.js
+++ yo-ordeno-react/src/app/components/Order/Step2.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import DishMenu from "./DishMenu";
 
@@ -6,7 +7,17 @@ const Step2 = ({dishesMenu, removeDishFromOrder, addDishToOrder, getQuantityOrde
   return (
     <div>
       <h1>Escoge lo que quieres ordenar</h1>
-      <ul uk-accordion="true">
+      <div uk-filter="target: .dish-filter">
+      <ul className="uk-subnav uk-subnav-pill">
+        <li uk-filter-control=""><a>Todos</a></li>
+        <li uk-filter-control=".tag-desayunos"><a>Desayunos</a></li>
+        <li uk-filter-control=".tag-entradas"><a>Entradas</a></li>
+        <li uk-filter-control=".tag-sopas"><a>Sopas</a></li>
+        <li uk-filter-control=".tag-ensaladas"><a>Ensaladas</a></li>
+        <li uk-filter-control=".tag-bebidas"><a>Bebidas</a></li>
+        <li uk-filter-control=".tag-platillo-principal"><a>Platillos principales</a></li>
+      </ul>
+      <ul uk-accordion="true" className="dish-filter">
         {dishesMenu &&
           dishesMenu.map((dish, index) => (
             <DishMenu
@@ -19,6 +30,7 @@ const Step2 = ({dishesMenu, removeDishFromOrder, addDishToOrder, getQuantityOrde
             />
           ))}
       </ul>
+      </div>
       <div className="uk-button-group">
         <button
           className="uk-button uk-button-default"
